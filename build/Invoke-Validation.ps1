253966674aca0a6cc0a32176542eec113eeb0f12
joerodgers <28455042+joerodgers@users.noreply.github.com>
2021-08-17 13:23:21 -0400

    Update Invoke-Validation.ps1

--- build/Invoke-Validation.ps1
+++ build/Invoke-Validation.ps1
@@ -1,5 +1,5 @@
 ﻿# define the module root
-$moduleRoot = Resolve-Path -Path "$PSScriptRoot" | Select-Object -ExpandProperty Path
+$moduleRoot = Resolve-Path -Path "$PSScriptRoot\.." | Select-Object -ExpandProperty Path
 
 # Run internal pester tests
-& "$moduleRoot\Tests\pester.ps1"
+& "$moduleRoot\TenantSiteMetadataSync\Tests\pester.ps1"
