cecc903507ad8b07b28545f63faeed7172fc8398
joerodgers <28455042+joerodgers@users.noreply.github.com>
2021-08-17 13:21:20 -0400

    Updated git workflow

--- build/Invoke-Validation.ps1
+++ build/Invoke-Validation.ps1
@@ -1,10 +1,5 @@
-﻿# Guide for available variables and working with secrets:
-# https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
-
-# Needs to ensure things are Done Right and only legal commits to master get built
-
-# define the module root
-$moduleRoot = Resolve-Path -Path "$PSScriptRoot\.." | Select-Object -ExpandProperty Path
+﻿# define the module root
+$moduleRoot = Resolve-Path -Path "$PSScriptRoot" | Select-Object -ExpandProperty Path
 
 # Run internal pester tests
 & "$moduleRoot\Tests\pester.ps1"
